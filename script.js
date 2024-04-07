932c7e9bc476ee61c602bd746a1a9189219b22f2
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-04-07 20:31:53 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+  alert("video Trailer Godzila x kong");
+});
+
+audio.addEventListener("click", function audio() {
+  alert(" Audio Trailer Godzila x Kong");
+});
+
+gambar.addEventListener("click", function gambar() {
+  alert(" Gambar Poster");
+});
